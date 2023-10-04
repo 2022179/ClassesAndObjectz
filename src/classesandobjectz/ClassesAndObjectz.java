3a11ad0884e19d90b38b277dcefccb0e3697b258
2022179 <146199008+2022179@users.noreply.github.com>
2023-10-04 12:32:09 +0100

    Initial commit

---
+++ src/classesandobjectz/ClassesAndObjectz.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package classesandobjectz;
+
+/**
+ *
+ * @author Primm
+ */
+public class ClassesAndObjectz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
